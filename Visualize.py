fb8ebb1ec8050112c44d325cc6872dec5c21ee07
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2021-03-16 13:17:53 +0300

    support csv and yolo4_darknet formats

--- Visualize.py
+++ Visualize.py
@@ -1,30 +1,34 @@
 import glob
 from GlobalConverter.Visualizer import Visualizer
 import os
+import cv2
 
 if __name__ == '__main__':
-    image_path = r'D:\02#OTOKAR\Projects\UT2001#Maske-Tespiti\total\images'
+    _type_ = "yolo4_darknet"
+    image_path = r'yolo4_darknet_test'
     dataset_name = "dataset.csv"
 
     FILTER = ["txt", "csv", "pbtxt"]
-    images = filter(lambda x: os.path.basename(x).split('.')[1] not in FILTER ,glob.glob(os.path.join(image_path, '*')))
+    images = filter(lambda x: os.path.basename(x).split('.')[-1] not in FILTER ,glob.glob(os.path.join(image_path, '*')))
 
-    print(os.path.join(image_path, dataset_name))
+    path = None
+    if _type_ == "csv":
+        path = os.path.join(image_path, dataset_name)
+    elif _type_ == "yolo4_darknet":
+        path = image_path
 
+    print("Path", path)
+    print("Type", _type_)
 
-    myVisualizer = Visualizer(os.path.join(image_path, dataset_name), "csv")
+    myVisualizer = Visualizer(path, _type_)
 
     colorMap = {
-        "Mask": (0,255,0),
-        "No-Mask": (0,0,255)
+        "0": (0,255,0),
+        "1": (0,0,255)
     }
+
     for i, image_path in enumerate(images):
         print("{:0>5} - {}".format(i+1, image_path))
-        myVisualizer.visualize(imagePath=image_path, stime=100, colorMap=colorMap)
-    
-    ## Visualize CSV Dataset
-    #Visualizer("../datasets/csv/dataset.csv", "csv", "../datasets/csv/000_1OC3DT.jpg").visualize()
+        myVisualizer.visualize(imagePath=image_path, stime=10, colorMap=colorMap)
 
-    ## Visualize Yolo4Darknet Dataset
-    #Visualizer('../datasets/yolo4_darknet', "yolo4_darknet", "../datasets/yolo4_darknet/test1.jpg").visualize(colorMap={"0":(0,255,0)})
     
